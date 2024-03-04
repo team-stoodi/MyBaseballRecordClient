d3567cadce15fc802ba21badb71d3dc202c74269
sangwoo <kajer102@gmail.com>
2024-03-04 14:19:41 +0900

    인트로페이지 버튼까지 작업, 아이콘은 아직입니다.

--- lib/common/auth_button.dart
+++ lib/common/auth_button.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AuthButton extends StatelessWidget {
-  const AuthButton({super.key});
+  final VoidCallback onClick;
+  final FaIcon icon;
+  final Color backgroundColor;
+  final Color? borderColor;
+  final Color? textColor;
+  final String text;
+
+  const AuthButton({
+    super.key,
+    required this.onClick,
+    required this.icon,
+    required this.backgroundColor,
+    required this.text,
+    this.borderColor,
+    this.textColor,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+      onPressed: onClick,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        side: borderColor != null ? BorderSide(color: borderColor!) : null,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          icon,
+          const SizedBox(width: 8),
+          Text(
+            text,
+            style: TextStyle(fontSize: 14, color: textColor ?? Colors.black),
+          ),
+        ],
+      ),
+    );
   }
 }
