d36a7fed0ae36bcd646f0bb9c37a8f1295c7ca90
sangwoo <kajer102@gmail.com>
2024-03-05 12:44:12 +0900

    AuthButton 수정

--- lib/common/auth_button.dart
+++ lib/common/auth_button.dart
@@ -3,40 +3,56 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AuthButton extends StatelessWidget {
   final VoidCallback onClick;
-  final FaIcon icon;
+  final IconData iconData;
   final Color backgroundColor;
   final Color? borderColor;
   final Color? textColor;
+  final Color? iconColor;
   final String text;
 
   const AuthButton({
     super.key,
     required this.onClick,
-    required this.icon,
+    required this.iconData,
     required this.backgroundColor,
     required this.text,
     this.borderColor,
     this.textColor,
+    this.iconColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onClick,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
-        side: borderColor != null ? BorderSide(color: borderColor!) : null,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          icon,
-          const SizedBox(width: 8),
-          Text(
-            text,
-            style: TextStyle(fontSize: 14, color: textColor ?? Colors.black),
+    return GestureDetector(
+      onTap: onClick,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(32.0),
+          border: Border.all(
+            color: borderColor ?? Colors.transparent,
           ),
-        ],
+          color: backgroundColor,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 10.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FaIcon(iconData, color: textColor ?? Colors.black),
+              const SizedBox(width: 8),
+              Text(
+                text,
+                style: TextStyle(
+                  color: textColor ?? Colors.black,
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
