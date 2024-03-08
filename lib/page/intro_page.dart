2ceedc7778294c4effc40a888d6a5562c6198266
sangwoo <kajer102@gmail.com>
2024-03-08 13:23:03 +0900

    IntroPage text 간격, 위젯간 거리 수정하였습니다.

--- lib/page/intro_page.dart
+++ lib/page/intro_page.dart
@@ -13,68 +13,67 @@ class IntroPage extends StatelessWidget {
       backgroundColor: AppColor.blackColor,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 32),
-              const Text(
-                '나의 오늘을',
-                style: TextStyle(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 67),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 32),
+                const Text(
+                  '나의 오늘을\n기록하다.',
+                  style: TextStyle(
                     color: AppColor.whiteColor,
                     fontSize: 48,
-                    fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                '기록하다.',
-                style: TextStyle(
-                    color: AppColor.whiteColor,
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                'My Baseball Record',
-                style: TextStyle(
-                    color: AppColor.whiteColor,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).size.height * 0.3),
-                child: const Spacer(),
-              ),
-              AuthButton(
+                    fontWeight: FontWeight.w900,
+                    height: 1.2,
+                  ),
+                ),
+                const SizedBox(height: 18),
+                const Text(
+                  'My Baseball Record',
+                  style: TextStyle(
+                      color: AppColor.whiteColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w900),
+                ),
+                // Padding(
+                //   padding: EdgeInsets.only(
+                //       bottom: MediaQuery.of(context).size.height * 0.3),
+                //   child: const Spacer(),
+                // ),
+                const SizedBox(height: 249),
+                AuthButton(
+                    onClick: () {},
+                    icon: const FaIcon(FontAwesomeIcons.apple),
+                    backgroundColor: AppColor.whiteColor,
+                    text: 'Apple로 계속하기'),
+                const SizedBox(height: 16),
+                AuthButton(
                   onClick: () {},
-                  icon: const FaIcon(FontAwesomeIcons.apple),
-                  backgroundColor: AppColor.whiteColor,
-                  text: 'Apple로 계속하기'),
-              const SizedBox(height: 8),
-              AuthButton(
-                onClick: () {},
-                icon: const FaIcon(FontAwesomeIcons.k),
-                backgroundColor: AppColor.yellowColor,
-                text: '카카오로 계속하기',
-              ),
-              const SizedBox(height: 8),
-              AuthButton(
-                  onClick: () {},
-                  icon: const FaIcon(FontAwesomeIcons.n),
-                  backgroundColor: AppColor.greenColor,
-                  text: '네이버로 계속하기'),
-              const SizedBox(height: 8),
-              AuthButton(
-                onClick: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const RegisterPage()));
-                },
-                icon: const FaIcon(FontAwesomeIcons.at),
-                backgroundColor: AppColor.blackColor,
-                borderColor: AppColor.whiteColor,
-                text: '이메일로 가입하기',
-                textColor: AppColor.whiteColor,
-              ),
-            ],
+                  icon: const FaIcon(FontAwesomeIcons.k),
+                  backgroundColor: AppColor.yellowColor,
+                  text: '카카오로 계속하기',
+                ),
+                const SizedBox(height: 16),
+                AuthButton(
+                    onClick: () {},
+                    icon: const FaIcon(FontAwesomeIcons.n),
+                    backgroundColor: AppColor.greenColor,
+                    text: '네이버로 계속하기'),
+                const SizedBox(height: 16),
+                AuthButton(
+                  onClick: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const RegisterPage()));
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.at),
+                  backgroundColor: AppColor.blackColor,
+                  borderColor: AppColor.whiteColor,
+                  text: '이메일로 가입하기',
+                  textColor: AppColor.whiteColor,
+                ),
+              ],
+            ),
           ),
         ),
       ),
