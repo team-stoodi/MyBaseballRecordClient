aacccba43f72677c9f708409cd6bd56bd74e340b
sangwoo <kajer102@gmail.com>
2024-03-13 08:09:55 +0900

    registerPage email텍스트필드와 유효성 검사 작동하는 것 작업입니다.

--- lib/common/auth_text_input_widget.dart
+++ lib/common/auth_text_input_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_baseball_record/common/app_color.dart';
+import 'package:my_baseball_record/common/app_text_style.dart';
 
 class AuthTextInputWidget extends StatelessWidget {
   final String labelText;
@@ -11,6 +12,7 @@ class AuthTextInputWidget extends StatelessWidget {
   final bool obscureText;
   final TextStyle? textStyle;
   final Function(String) onChanged;
+  final VoidCallback onEditingComplete;
 
   const AuthTextInputWidget({
     super.key,
@@ -23,47 +25,69 @@ class AuthTextInputWidget extends StatelessWidget {
     this.obscureText = false,
     this.textStyle,
     required this.onChanged,
+    required this.onEditingComplete,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      height: 86,
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
       decoration: BoxDecoration(
         color: AppColor.background246,
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(8),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Stack(
         children: [
-          Text(
-            labelText,
-            style: const TextStyle(
-                color: AppColor.primaryBlue1,
-                fontSize: 12,
-                fontWeight: FontWeight.w400),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Text(
+              labelText,
+              style:
+                  AppTextStyle.body413M.copyWith(color: AppColor.primaryBlue1),
+            ),
           ),
-          TextField(
-            style: textStyle,
-            keyboardType: keyboardType,
-            obscureText: obscureText,
-            controller: controller,
-            onChanged: onChanged,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: hintText,
-              hintStyle: const TextStyle(
-                fontSize: 20,
-                color: AppColor.textPrimary10,
-                fontWeight: FontWeight.bold,
-              ),
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.cancel),
-                color: isTextNotEmpty ? AppColor.textHint : AppColor.textHint,
-                onPressed: onClearPressed,
-              ),
+          Positioned(
+            top: 16,
+            bottom: -10,
+            left: 0,
+            right: -15,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: TextField(
+                    onEditingComplete: onEditingComplete,
+                    textInputAction: TextInputAction.next,
+                    autocorrect: false,
+                    cursorHeight: 24,
+                    style: textStyle,
+                    keyboardType: keyboardType,
+                    obscureText: obscureText,
+                    controller: controller,
+                    onChanged: onChanged,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.zero,
+                      border: InputBorder.none,
+                      hintText: hintText,
+                      hintStyle: AppTextStyle.body120M
+                          .copyWith(color: AppColor.textPrimary20),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: onClearPressed,
+                  icon: Icon(
+                    Icons.cancel,
+                    size: 20,
+                    color:
+                        isTextNotEmpty ? AppColor.textHint : AppColor.textHint,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/page/register_page.dart
+++ lib/page/register_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_baseball_record/common/app_color.dart';
+import 'package:my_baseball_record/common/app_text_list.dart';
+import 'package:my_baseball_record/common/app_text_style.dart';
 import 'package:my_baseball_record/common/auth_text_input_widget.dart';
-import 'package:my_baseball_record/common/sticky_bottom_button.dart';
 import 'package:my_baseball_record/common/util/validate.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -19,15 +20,8 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  bool isEmailValid = true;
-  bool isPasswordValid = true;
 
-  @override
-  void initState() {
-    super.initState();
-    emailController.addListener(updateStatus);
-    passwordController.addListener(updateStatus);
-  }
+  String? emailError;
 
   @override
   void dispose() {
@@ -37,10 +31,16 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   void updateStatus() {
-    setState(() {
-      isEmailValid = validateEmail(emailController.text);
-      isPasswordValid = validatePassword(passwordController.text);
-    });
+    final email = emailController.text;
+    if (!validateEmail(email)) {
+      setState(() {
+        emailError = AppTextList.criticalAlertEmailText;
+      });
+    } else {
+      setState(() {
+        emailError = null;
+      });
+    }
   }
 
   @override
@@ -50,94 +50,59 @@ class _RegisterPageState extends State<RegisterPage> {
         child: Stack(
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     children: [
-                      const Text(
-                        '회원가입',
-                        style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.bold),
+                      Text(
+                        AppTextList.registerText,
+                        style: AppTextStyle.h224B
+                            .copyWith(color: AppColor.textPrimary),
                       ),
-                      const Text(
-                        ' | ',
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: AppColor.graysGray,
-                        ),
+                      Text(
+                        AppTextList.divider,
+                        style: AppTextStyle.h224B
+                            .copyWith(color: AppColor.graysGray),
                       ),
                       GestureDetector(
                         onTap: widget.toggleAuthMode,
-                        child: const Text(
-                          '로그인',
-                          style: TextStyle(
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold,
-                              color: AppColor.textPrimary20),
+                        child: Text(
+                          AppTextList.loginText,
+                          style: AppTextStyle.h224B
+                              .copyWith(color: AppColor.textPrimary20),
                         ),
                       ),
                     ],
                   ),
                   const SizedBox(height: 32),
                   AuthTextInputWidget(
-                    textStyle: const TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.bold),
-                    labelText: '이메일',
-                    hintText: '이메일',
+                    textStyle: AppTextStyle.body120M
+                        .copyWith(color: AppColor.textPrimary),
+                    labelText: AppTextList.emailLabel,
+                    hintText: AppTextList.emailLabel,
                     controller: emailController,
                     onClearPressed: () => emailController.clear(),
                     isTextNotEmpty: emailController.text.isNotEmpty,
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: (String value) {},
-                  ),
-                  if (!isEmailValid)
-                    const Padding(
-                      padding: EdgeInsets.only(top: 8),
-                      child: Text(
-                        '이메일을 정확하게 입력해주세요.',
-                        style: TextStyle(color: AppColor.redColor),
-                      ),
-                    ),
-                  const SizedBox(height: 8),
-                  if (isEmailValid)
-                    const Text('비밀번호 찾기와 같은 꼭 필요한 안내 메일이 발송됩니다.'),
-                  const SizedBox(height: 16),
-                  AuthTextInputWidget(
-                    textStyle: const TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.bold),
-                    labelText: '비밀번호',
-                    hintText: '비밀번호',
-                    controller: passwordController,
-                    onClearPressed: () => passwordController.clear(),
-                    isTextNotEmpty: passwordController.text.isNotEmpty,
                     keyboardType: TextInputType.text,
-                    obscureText: true,
                     onChanged: (String value) {},
+                    onEditingComplete: updateStatus,
                   ),
                   const SizedBox(height: 8),
-                  if (!isPasswordValid)
-                    const Text(
-                      '영문/숫자/특수문자를 조합한 8-16자로 확인해주세요.',
-                      style: TextStyle(color: AppColor.redColor),
+                  if (emailError == null)
+                    Text(
+                      AppTextList.importantNotificationEmailText,
+                      style: AppTextStyle.caption213R
+                          .copyWith(color: AppColor.textHint),
                     ),
-                  if (isPasswordValid)
-                    const Text(
-                      '영문/숫자/특수문자를 8-16자리로 조합해주세요.',
+                  if (emailError != null)
+                    Text(
+                      emailError!,
+                      style: AppTextStyle.caption213R
+                          .copyWith(color: AppColor.accentRed100),
                     ),
                 ],
-              ),
-            ),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: StickyBottomButton(
-                text: '바로 시작하기',
-                onClick: () {},
-                enabled: isEmailValid && isPasswordValid,
               ),
             ),
           ],
